4358e95bc344fed7ae9c724dc41b1ba0912ef989
Thiago Henrique Heinle Sanchez <thiagohenque@live.com>
2024-10-03 19:13:39 -0300

    inserindo os nome na lista

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,16 +1,32 @@
 
 import express from "express";
+import { readFile, promises } from "fs";
+
+const dataSource = './data/list.txt'
 
 
 const router = express.Router();
 
-router.post('/contato', (req, res) => {
+router.post('/contato', async (req, res) => {
     const {name} = req.body;
 
     if (!name || name.length < 2  ) {
          res.json({error: 'Nome é obrigatório'});  
          return ;
     }
+
+    let list: string[] = [];
+    try {
+        const data = await promises.readFile(dataSource, 'utf8');
+        list = data.split('\n');
+    } catch (err) {
+        console.error(err);
+    }
+
+    list.push(name);
+
+    await promises.writeFile(dataSource, list.join('\n'));
+
 
     res.status(201).json ({contato: name})
 
